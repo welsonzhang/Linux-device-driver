9b84c2ccacd9d975e4c5c4214a435d8c148fc1e8
welsonzhang <942469560@qq.com>
2016-02-09 00:20:09 +0800

    wordcount

---
+++ wordcount/.tmp_versions/wordcount.mod
@@ -0,0 +1,2 @@
+/root/git/Linux-device-driver/wordcount/wordcount.ko
+/root/git/Linux-device-driver/wordcount/wordcount.o
---
+++ wordcount/.wordcount.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/root/git/Linux-device-driver/wordcount/wordcount.ko := ld -r -m elf_x86_64 -T /usr/src/kernels/3.10.0-327.3.1.el7.x86_64/scripts/module-common.lds --build-id  -o /root/git/Linux-device-driver/wordcount/wordcount.ko /root/git/Linux-device-driver/wordcount/wordcount.o /root/git/Linux-device-driver/wordcount/wordcount.mod.o
